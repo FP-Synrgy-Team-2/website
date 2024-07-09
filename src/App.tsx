87326570678d877b379f87dec5c59d6f10366155
Chery <cherylene40@gmail.com>
2024-07-09 22:47:13 +0700

    first commit: setup project configuration - tailwind, vite, react ts

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+// import { useState } from 'react'
+
+function App() {
+  return (
+    <>
+      <h1 className="text-lg font-bold underline text-center">Hello world!</h1> 
+
+    </>
+  )
+}
+
+export default App
