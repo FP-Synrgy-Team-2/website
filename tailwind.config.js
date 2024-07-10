fa26ef796d4d391ee44197a0d38921baff979fbb
hatrnuhn <yusron.ibnunaim@gmail.com>
2024-07-11 01:28:02 +0700

    feat: extends colors and classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,30 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'pr-blue': '#0066AE',
+        'pr-light-blue': '#E4EDFF',
+        'neutral-02': '#F5F5F5',
+        'neutral-03': '#B3B3B3',
+        'neutral-09': '#1C1C1E',
+        'sc-red': '#CB3A31',
+        'sc-green': '#12D79C',
+        'sc-yellow': '#FFB831',
+        'ac-dark-grey': '#5D5D5D'
+      },
+      width: {
+        '12.5': '3.125rem',
+        '28': '6.25rem'
+      },
+      height: {
+        '12.5': '3.125rem',
+        '27.5': '6.875rem'
+      },
+      padding: {
+        '4.25': '1.0625rem'
+      }
+    },
     screens: {
       xl: { max: "1279px" },
       // => @media (max-width: 1279px) { ... }
