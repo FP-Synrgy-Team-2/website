0aff2ec7a55c38d795ed5011d644109d5f4c060b
XisUndefined <muhlisahmadabdillah_1306619028@mhs.unj.ac.id>
2024-07-14 19:50:22 +0700

    feat: add transfer page with dropdown functionality
    - public/: add icons
    - layouts/components: moved Sidebar component to general component
    - types/saved-accounts.ts: add saved accounts data type
    - pages: add transfer page

--- src/pages/transfer/Saved.tsx
+++ src/pages/transfer/Saved.tsx
@@ -1,7 +1,17 @@
+import { useLocation } from "react-router-dom";
+import { SavedAccount } from "../../types/saved-accounts";
+
 function Saved(){
+    const location = useLocation()
     return (
         <div>
             <h1>Saved</h1>
+            {location.state ? (
+                <>
+                <p>{(location.state.account as SavedAccount).owner_name}</p>
+                <p>{(location.state.account as SavedAccount).account_number}</p>
+                </>
+            ) : ''}
         </div>
     )
 }
