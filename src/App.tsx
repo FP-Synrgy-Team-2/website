9f58968d3c5f1c2392dd7a0e43b33adb146c1e99
TCherylene <cherylene40@gmail.com>
2024-07-15 13:50:51 +0700

    Rebasing: update routes in app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,52 @@
-import { Route, Routes } from 'react-router-dom';
-import DashboardLayouts from './layouts/DashboardLayouts';
+import { Route, Routes } from "react-router-dom"
+import DashboardLayouts from "./layouts/DashboardLayouts"
 
-import SwaggerUIComponent from './components/SwaggerUI';
+import { SwaggerUIComponent, Protected } from './components'
 
 import {
-  ApiCall,
-  Dashboard,
-  Login,
-  History,
-  Confirmation,
-  New,
-  Pin,
-  Receipt,
-  Saved,
-  TransferPage,
-} from './pages';
+    ApiCall,
+    Dashboard, Login,
+    History,
+    Confirmation,
+    New,
+    Pin,
+    Receipt,
+    Saved,
+    TransferPage,
+} from "./pages"
 
 function App() {
-  return (
-    <>
-      <Routes>
-        <Route index element={<DashboardLayouts />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/logout" />
+    return (
+        <>
+            <Routes>
+                <Route path="/login" element={<Login />} />
+                <Route path="/logout" />
 
-        <Route path="" element={<DashboardLayouts />}>
-          <Route path="dashboard" element={<Dashboard />} />
+                <Route path="" element={
+                    <Protected>
+                        <DashboardLayouts />
+                    </Protected>
+                }>
+                    <Route path="dashboard" element={<Dashboard />} />
+                    <Route path="transfer">
+                        <Route index element={<TransferPage />} />
+                        <Route path="new" element={<New />} />
+                        <Route path="saved" element={<Saved />} />
+                        <Route path="confirm" element={<Confirmation />} />
+                        <Route path="pin" element={<Pin />} />
+                        <Route path="receipt" element={<Receipt />} />
+                    </Route>
+                    <Route path="history" element={<History />} />
+                </Route>
 
-          <Route path="transfer">
-            <Route index element={<TransferPage />} />
-            <Route path="new" element={<New />} />
-            <Route path="saved" element={<Saved />} />
-            <Route path="confirm" element={<Confirmation />} />
-            <Route path="pin" element={<Pin />} />
-            <Route path="receipt" element={<Receipt />} />
-          </Route>
+                <Route path="/api">
+                    <Route path="example" element={<ApiCall />}></Route>
+                    <Route path="docs" element={<SwaggerUIComponent />} />
+                </Route>
 
-          <Route path="history">
-            <Route index element={<History />} />
-          </Route>
-        </Route>
-
-        <Route path="/api">
-          <Route path="example" element={<ApiCall />}></Route>
-          <Route path="docs" element={<SwaggerUIComponent />} />
-        </Route>
-      </Routes>
-    </>
-  );
+            </Routes>
+        </>
+    )
 }
 
-export default App;
+export default App
