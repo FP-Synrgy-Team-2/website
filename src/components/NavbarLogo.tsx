a196dd56627d5ee0731ee4bb449e37f043617b9f
TCherylene <cherylene40@gmail.com>
2024-07-15 13:50:35 +0700

    Rebasing: Add components

--- src/components/NavbarLogo.tsx
+++ src/components/NavbarLogo.tsx
@@ -1,11 +1,13 @@
-function NavbarLogo() {
-  return (
-    <img
-      className="navbar-brand"
-      src="/images/logo.png"
-      alt="Icon jangkau by BCA"
-    />
-  );
+interface NavbarLogoProps {
+    className?: string;
+}
+
+function NavbarLogo({className}: NavbarLogoProps) {
+    return (
+        <img className={`navbar-brand ${className}`} 
+        src="/images/logo.png" 
+        alt="Icon jangkau by BCA" />
+    )
 }
 
 export default NavbarLogo;
