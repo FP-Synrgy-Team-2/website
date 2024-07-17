fadf0523f32a6c5e2a288a0f9242742d6d1165fe
Charles <charlesgunawan32@gmail.com>
2024-07-17 20:06:59 +0700

    add saldo card and mutasi table for dashboard

--- src/components/Saldo/TableMutasi.tsx
+++ src/components/Saldo/TableMutasi.tsx
@@ -1,86 +1,123 @@
-import sortArrow from "../../assets/icons/Sort_arrow.png"
+import React from 'react';
+import sortArrow from '../../assets/icons/Sort_arrow.png';
 
-function TableMutasi() {
+const TableMutasi: React.FC = () => {
+  function formatNumber(num: number): string {
+    return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1.');
+  }
 
-    function formatNumber(num: number) {
-        return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1.');
-    }
-
-    return (
-        <div className=" w-full  h-full">
-            <div className="flex justify-between items-center">
-                <h1 className="uppercase font-xl-body" aria-label="mutasi rekening">Mutasi Rekening</h1>
-                <button className="p-2 bg-primary-blue text-neutral-01 rounded-lg font-[550] uppercase font-lg-body" aria-label="tombol tampilkan semua">Tampilkan Semua</button>
+  return (
+    <div className="h-full w-full">
+      <div className="flex items-center justify-between">
+        <h1 className="font-xl-body uppercase" aria-label="mutasi rekening">
+          Mutasi Rekening
+        </h1>
+        <button
+          className="font-lg-body rounded-lg bg-primary-blue p-2 font-[550] uppercase text-neutral-01"
+          aria-label="tombol tampilkan semua"
+        >
+          Tampilkan Semua
+        </button>
+      </div>
+      <div className="mt-2 h-auto rounded-lg border border-grey py-7">
+        <div className="mx-auto flex w-10/12 flex-col border-b py-4">
+          <div className="mb-2 flex items-center gap-2">
+            <div className="flex h-6 w-6 items-center justify-center rounded-full bg-primary-light-blue text-primary-blue">
+              <img src={sortArrow} alt="Arrow" />
             </div>
-            <div className="border border-grey rounded-lg h-auto mt-2 py-7">
-            <div className="w-10/12 mx-auto flex flex-col py-4 border-b ">
-                <div className="flex gap-2 items-center mb-2">
-                    <div className="w-6 h-6 rounded-full bg-primary-light-blue text-primary-blue flex items-center justify-center">
-                    <img src={sortArrow} alt="Arrow" />
-                    </div>
-                    <p className="font-bold text-lg-body" aria-label="Transfer">Transfer</p>
-                </div>
-                <div className="flex gap-2 items-center justify-between text-md-body uppercase font-regular">
-                    <p>bca</p>
-                    <p className=" normal-case" aria-label={"Rp. "}>Rp. {formatNumber(2000000)}</p>
-                    <p className="text-grey" aria-label="tanggal"> tanggal</p>
-                </div>
-            </div>
-            <div className="w-10/12 mx-auto flex flex-col py-4 border-b ">
-                <div className="flex gap-2 items-center mb-2">
-                    <div className="w-6 h-6 rounded-full bg-primary-light-blue text-primary-blue flex items-center justify-center">
-                    <img src={sortArrow} alt="Arrow" />
-                    </div>
-                    <p className="font-bold text-lg-body" aria-label="Transfer">Transfer</p>
-                </div>
-                <div className="flex gap-2 items-center justify-between text-md-body uppercase font-regular">
-                    <p>bca</p>
-                    <p className=" normal-case" aria-label={"Rp. "}>Rp. {formatNumber(2000000)}</p>
-                    <p className="text-grey" aria-label="tanggal"> tanggal</p>
-                </div>
-            </div>
-            <div className="w-10/12 mx-auto flex flex-col py-4 border-b ">
-                <div className="flex gap-2 items-center mb-2">
-                    <div className="w-6 h-6 rounded-full bg-primary-light-blue text-primary-blue flex items-center justify-center">
-                    <img src={sortArrow} alt="Arrow" />
-                    </div>
-                    <p className="font-bold text-lg-body" aria-label="Transfer">Transfer</p>
-                </div>
-                <div className="flex gap-2 items-center justify-between text-md-body uppercase font-regular">
-                    <p>bca</p>
-                    <p className=" normal-case" aria-label={"Rp. "}>Rp. {formatNumber(2000000)}</p>
-                    <p className="text-grey" aria-label="tanggal"> tanggal</p>
-                </div>
-            </div>
-            <div className="w-10/12 mx-auto flex flex-col py-4 border-b ">
-                <div className="flex gap-2 items-center mb-2">
-                    <div className="w-6 h-6 rounded-full bg-primary-light-blue text-primary-blue flex items-center justify-center">
-                    <img src={sortArrow} alt="Arrow" />
-                    </div>
-                    <p className="font-bold text-lg-body" aria-label="Transfer">Transfer</p>
-                </div>
-                <div className="flex gap-2 items-center justify-between text-md-body uppercase font-regular">
-                    <p>bca</p>
-                    <p className=" normal-case" aria-label={"Rp. "}>Rp. {formatNumber(2000000)}</p>
-                    <p className="text-grey" aria-label="tanggal"> tanggal</p>
-                </div>
-            </div>
-            <div className="w-10/12 mx-auto flex flex-col py-4 border-b ">
-                <div className="flex gap-2 items-center mb-2">
-                    <div className="w-6 h-6 rounded-full bg-primary-light-blue text-primary-blue flex items-center justify-center">
-                    <img src={sortArrow} alt="Arrow" />
-                    </div>
-                    <p className="font-bold text-lg-body" aria-label="Transfer">Transfer</p>
-                </div>
-                <div className="flex gap-2 items-center justify-between text-md-body uppercase font-regular">
-                    <p>bca</p>
-                    <p className=" normal-case" aria-label={"Rp. "}>Rp. {formatNumber(2000000)}</p>
-                    <p className="text-grey" aria-label="tanggal"> tanggal</p>
-                </div>
-            </div>
-            </div>
+            <p className="text-lg-body font-bold" aria-label="Transfer">
+              Transfer
+            </p>
+          </div>
+          <div className="flex items-center justify-between gap-2 text-md-body font-regular uppercase">
+            <p>bca</p>
+            <p className="normal-case" aria-label={'Rp. '}>
+              Rp. {formatNumber(2000000)}
+            </p>
+            <p className="text-grey" aria-label="tanggal">
+              tanggal
+            </p>
+          </div>
         </div>
-    );
-}
+        <div className="mx-auto flex w-10/12 flex-col border-b py-4">
+          <div className="mb-2 flex items-center gap-2">
+            <div className="flex h-6 w-6 items-center justify-center rounded-full bg-primary-light-blue text-primary-blue">
+              <img src={sortArrow} alt="Arrow" />
+            </div>
+            <p className="text-lg-body font-bold" aria-label="Transfer">
+              Transfer
+            </p>
+          </div>
+          <div className="flex items-center justify-between gap-2 text-md-body font-regular uppercase">
+            <p>bca</p>
+            <p className="normal-case" aria-label={'Rp. '}>
+              Rp. {formatNumber(2000000)}
+            </p>
+            <p className="text-grey" aria-label="tanggal">
+              tanggal
+            </p>
+          </div>
+        </div>
+        <div className="mx-auto flex w-10/12 flex-col border-b py-4">
+          <div className="mb-2 flex items-center gap-2">
+            <div className="flex h-6 w-6 items-center justify-center rounded-full bg-primary-light-blue text-primary-blue">
+              <img src={sortArrow} alt="Arrow" />
+            </div>
+            <p className="text-lg-body font-bold" aria-label="Transfer">
+              Transfer
+            </p>
+          </div>
+          <div className="flex items-center justify-between gap-2 text-md-body font-regular uppercase">
+            <p>bca</p>
+            <p className="normal-case" aria-label={'Rp. '}>
+              Rp. {formatNumber(2000000)}
+            </p>
+            <p className="text-grey" aria-label="tanggal">
+              tanggal
+            </p>
+          </div>
+        </div>
+        <div className="mx-auto flex w-10/12 flex-col border-b py-4">
+          <div className="mb-2 flex items-center gap-2">
+            <div className="flex h-6 w-6 items-center justify-center rounded-full bg-primary-light-blue text-primary-blue">
+              <img src={sortArrow} alt="Arrow" />
+            </div>
+            <p className="text-lg-body font-bold" aria-label="Transfer">
+              Transfer
+            </p>
+          </div>
+          <div className="flex items-center justify-between gap-2 text-md-body font-regular uppercase">
+            <p>bca</p>
+            <p className="normal-case" aria-label={'Rp. '}>
+              Rp. {formatNumber(2000000)}
+            </p>
+            <p className="text-grey" aria-label="tanggal">
+              tanggal
+            </p>
+          </div>
+        </div>
+        <div className="mx-auto flex w-10/12 flex-col border-b py-4">
+          <div className="mb-2 flex items-center gap-2">
+            <div className="flex h-6 w-6 items-center justify-center rounded-full bg-primary-light-blue text-primary-blue">
+              <img src={sortArrow} alt="Arrow" />
+            </div>
+            <p className="text-lg-body font-bold" aria-label="Transfer">
+              Transfer
+            </p>
+          </div>
+          <div className="flex items-center justify-between gap-2 text-md-body font-regular uppercase">
+            <p>bca</p>
+            <p className="normal-case" aria-label={'Rp. '}>
+              Rp. {formatNumber(2000000)}
+            </p>
+            <p className="text-grey" aria-label="tanggal">
+              tanggal
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default TableMutasi;
