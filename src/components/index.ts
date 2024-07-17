11731a7bcbe4c15bc4d777c4752a960d869603cd
Chery <cherylene40@gmail.com>
2024-07-17 08:26:56 +0700

    update receipt

---
+++ src/components/index.ts
@@ -0,0 +1,23 @@
+import Breadcrumbs from './Breadcrumbs';
+import Button from './Button';
+import Card from './Card';
+import Checkbox from './Checkbox';
+import ModalBase from './ModalBase';
+import ModalFail from './ModalFail';
+import ModalSuccess from './ModalSuccess';
+import NavbarLogo from './NavbarLogo';
+import Sidebar from './Sidebar';
+import SwaggerUIComponent from './SwaggerUI';
+
+export {
+    Breadcrumbs,
+    Button,
+    Card,
+    Checkbox,
+    ModalBase,
+    ModalFail,
+    ModalSuccess,
+    NavbarLogo,
+    Sidebar,
+    SwaggerUIComponent,
+};
--- src/layouts/DashboardLayouts.tsx
+++ src/layouts/DashboardLayouts.tsx
@@ -1,17 +1,17 @@
 import { Outlet } from 'react-router-dom';
-import Sidebar from '../components/Sidebar';
+import { Sidebar } from '../components';
 
 function DashboardLayouts() {
-  return (
-    <div className="flex">
-      <aside className="container flex h-screen basis-1/6 flex-col px-3 pb-16 pt-5">
-        <Sidebar />
-      </aside>
-      <main className="container min-h-screen basis-5/6 bg-body-white ps-8 pt-5">
-        <Outlet />
-      </main>
-    </div>
-  );
+    return (
+        <div className="flex">
+            <aside className="container flex h-screen basis-1/6 flex-col px-3 pb-16 pt-5">
+                <Sidebar />
+            </aside>
+            <main className="container min-h-screen basis-5/6 bg-body-white ps-8 pt-5">
+                <Outlet />
+            </main>
+        </div>
+    );
 }
 
 export default DashboardLayouts;
