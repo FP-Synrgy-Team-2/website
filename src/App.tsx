714bd33cd1b4502375d2b5486a2cd4808da35c71
Chery <cherylene40@gmail.com>
2024-07-17 08:26:56 +0700

    update receipt

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,58 @@
+/* eslint-disable prettier/prettier */
 import { Route, Routes } from 'react-router-dom';
 import DashboardLayouts from './layouts/DashboardLayouts';
+import ReceiptLayouts from './layouts/ReceiptLayouts';
 
 import SwaggerUIComponent from './components/SwaggerUI';
 
 import {
-  ApiCall,
-  Dashboard,
-  Login,
-  History,
-  Confirmation,
-  New,
-  Pin,
-  Receipt,
-  Saved,
-  TransferPage,
+    ApiCall,
+    Dashboard,
+    Login,
+    History,
+    Confirmation,
+    New,
+    Pin,
+    Receipt,
+    Saved,
+    TransferPage,
 } from './pages';
 
 function App() {
-  return (
-    <>
-      <Routes>
-        <Route index element={<DashboardLayouts />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/logout" />
+    return (
+        <>
+            <Routes>
+                <Route index element={<DashboardLayouts />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/logout" />
 
-        <Route path="" element={<DashboardLayouts />}>
-          <Route path="dashboard" element={<Dashboard />} />
+                <Route path="" element={<DashboardLayouts />}>
+                    <Route path="dashboard" element={<Dashboard />} />
 
-          <Route path="transfer">
-            <Route index element={<TransferPage />} />
-            <Route path="new" element={<New />} />
-            <Route path="saved" element={<Saved />} />
-            <Route path="confirm" element={<Confirmation />} />
-            <Route path="pin" element={<Pin />} />
-            <Route path="receipt" element={<Receipt />} />
-          </Route>
+                    <Route path="transfer">
+                        <Route index element={<TransferPage />} />
+                        <Route path="new" element={<New />} />
+                        <Route path="saved" element={<Saved />} />
+                        <Route path="confirm" element={<Confirmation />} />
+                        <Route path="pin" element={<Pin />} />
+                    </Route>
 
-          <Route path="history">
-            <Route index element={<History />} />
-          </Route>
-        </Route>
+                    <Route path="history">
+                        <Route index element={<History />} />
+                    </Route>
+                </Route>
 
-        <Route path="/api">
-          <Route path="example" element={<ApiCall />}></Route>
-          <Route path="docs" element={<SwaggerUIComponent />} />
-        </Route>
-      </Routes>
-    </>
-  );
+                <Route path="transfer/receipt" element={<ReceiptLayouts />}>
+                    <Route path=":id" element={<Receipt />} />
+                </Route>
+
+                <Route path="/api">
+                    <Route path="example" element={<ApiCall />}></Route>
+                    <Route path="docs" element={<SwaggerUIComponent />} />
+                </Route>
+            </Routes>
+        </>
+    );
 }
 
 export default App;
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -2,79 +2,102 @@ import NavbarLogo from './NavbarLogo';
 import { Link, useLocation } from 'react-router-dom';
 
 interface NavbarType {
-  name: string;
-  icon: string;
-  link: string;
-  className?: string;
-  alt?: string;
+    name: string;
+    icon: string;
+    link: string;
+    className?: string;
+    alt?: string;
 }
 
 function Sidebar() {
-  const location = useLocation();
-  const navbar: NavbarType[] = [
-    {
-      name: 'Beranda',
-      icon: '/images/icons/home.svg',
-      link: '/',
-      alt: 'Logo Beranda',
-    },
-    {
-      name: 'Transfer',
-      icon: '/images/icons/sort-arrow.svg',
-      link: '/transfer',
-      alt: 'Logo Transfer',
-    },
-    {
-      name: 'Mutasi',
-      icon: '/images/icons/print.svg',
-      link: '/history',
-      alt: 'Logo Mutasi',
-    },
-    {
-      name: 'Profil',
-      icon: '/images/icons/user.svg',
-      link: '/profile',
-      className: 'mt-auto',
-      alt: 'Logo Profil',
-    },
-    {
-      name: 'Keluar',
-      icon: '/images/icons/arrow-right.svg',
-      link: '/logout',
-      className: 'text-danger hover:bg-danger hover:text-white',
-      alt: 'Logo Keluar',
-    },
-  ];
+    const location = useLocation();
+    const navbar: NavbarType[] = [
+        {
+            name: 'Beranda',
+            icon: '/images/icons/home.svg',
+            link: '/',
+            alt: 'Logo Beranda',
+        },
+        {
+            name: 'Transfer',
+            icon: '/images/icons/sort-arrow.svg',
+            link: '/transfer',
+            alt: 'Logo Transfer',
+        },
+        {
+            name: 'Mutasi',
+            icon: '/images/icons/print.svg',
+            link: '/history',
+            alt: 'Logo Mutasi',
+        },
+        {
+            name: 'Profil',
+            icon: '/images/icons/user.svg',
+            link: '/profile',
+            className: 'mt-auto',
+            alt: 'Logo Profil',
+        },
+        {
+            name: 'Keluar',
+            icon: '/images/icons/arrow-right.svg',
+            link: '/logout',
+            className: 'text-danger hover:bg-danger hover:text-white',
+            alt: 'Logo Keluar',
+        },
+    ];
 
-  return (
-    <>
-      <div className="ms-3">
-        <NavbarLogo />
-      </div>
+    return (
+        <>
+            <div className="ms-3">
+                <NavbarLogo />
+            </div>
 
-      <div className="mt-8 flex grow flex-col">
-        {navbar.map((item, index) => {
-          const isActive: boolean = location.pathname === item.link;
+            <div className="mt-8 flex grow flex-col">
+                {navbar.map((item, index) => {
+                    const isActive: boolean = location.pathname === item.link;
 
-          return (
-            <Link
-              to={item.link}
-              key={index}
-              className={`sidebar-item flex items-center gap-5 rounded-md p-3 ${isActive ? 'bg-primary-blue text-white' : 'hover:bg-primary-dark-blue hover:text-white'} ${item.className ? item.className : ''}`}
-              aria-label={'Tombol ' + item.name}
-            >
-              <img
-                src={item.icon}
-                aria-hidden={true}
-                className={`${isActive ? 'img-white' : ''}`}
-              />
-              <span aria-hidden={true}>{item.name}</span>
-            </Link>
-          );
-        })}
-      </div>
-    </>
-  );
+                    return (
+                        <Link
+                            to={item.link}
+                            key={index}
+                            className={`sidebar-item flex items-center gap-5 rounded-md p-3 text-xs-display ${
+                                isActive
+                                    ? 'bg-primary-blue text-white'
+                                    : 'hover:bg-primary-dark-blue hover:text-white'
+                            } ${item.className ? item.className : ''}`}
+                            aria-label={'Tombol ' + item.name}
+                        >
+                            <img
+                                src={item.icon}
+                                aria-hidden={true}
+                                className={`${isActive ? 'img-white' : ''}`}
+                            />
+                            <span
+                                className="text-xs-display"
+                                aria-hidden={true}
+                            >
+                                {item.name}
+                            </span>
+                        </Link>
+                    );
+                })}
+
+                <button
+                    onClick={handleLogout}
+                    className={`sidebar-item flex items-center gap-5 rounded-md p-3 text-xs-display hover:bg-danger hover:text-white`}
+                >
+                    <img
+                        src="/images/icons/arrow-right.svg"
+                        alt="Logo Keluar"
+                        aria-hidden={true}
+                    />
+                    <span className="text-xs-display" aria-hidden={true}>
+                        Keluar
+                    </span>
+                </button>
+            </div>
+        </>
+    );
 }
 
 export default Sidebar;
