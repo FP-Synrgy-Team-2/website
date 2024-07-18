646d975b63733cf67ce4e1e206c2cf3fca120fcb
Chery <cherylene40@gmail.com>
2024-07-19 04:14:52 +0700

    feat: add receipt pages

--- src/pages/transfer/Receipt.tsx
+++ src/pages/transfer/Receipt.tsx
@@ -1,108 +1,81 @@
-import { useParams } from 'react-router-dom';
-import { NavbarLogo } from '../../components';
-import axios from 'axios';
-import { useEffect, useState } from 'react';
+import { Breadcrumbs, Button } from '@/components';
 
 function Receipt() {
-  const URL = import.meta.env.VITE_API_URL;
-  const { id } = useParams();
-  const [senderAccount, setSenderAccount] = useState(null);
-  const [receiverAccount, setReceiverAccount] = useState(null);
-  const [receiverName, setReceiverName] = useState(null);
-  const [amount, setAmount] = useState(0);
-  const [adminFee, setAdminFee] = useState(0);
-  const [transactionDate, setTransactionDate] = useState(null);
-  const [note, setNote] = useState(null);
-  const [total, setTotal] = useState(0);
-
-  function formatRupiah(amount: number): string {
-    return new Intl.NumberFormat('id-ID', {
-      style: 'currency',
-      currency: 'IDR',
-      minimumFractionDigits: 0,
-      maximumFractionDigits: 0,
-    }).format(amount);
-  }
-
-  useEffect(() => {
-    // axios get /transaction/:id
-    axios
-      .get(`${URL}/transaction/${id}`)
-      .then((res) => {
-        const result = res.data;
-
-        setSenderAccount(result.account_id);
-        setReceiverAccount(result.beneficiary_account);
-        // setReceiverName(result.nama_tujuan);
-        setTransactionDate(result.transaction_date);
-        setAmount(result.amount);
-        setAdminFee(result.admin_fee);
-        setNote(result.note);
-        setTotal(result.total);
-      })
-      .catch((err) => {
-        console.error(err);
-      });
-
-    // axios get bank-accounts/:account_number
-    axios
-      .get(`${URL}/bank-accounts/${receiverAccount}`)
-      .then((res) => {
-        const result = res.data;
-
-        setReceiverName(result.owner_name);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }, [
-    senderAccount,
-    receiverAccount,
-    receiverName,
-    amount,
-    adminFee,
-    transactionDate,
-    note,
-    total,
-  ]);
+  const breadcrumbs = [
+    { label: 'Transfer', path: '/transfer' },
+    { label: 'Input Data Transfer', path: '/transfer/new' },
+  ];
 
   return (
     <>
-      <div className="z-10 mx-auto w-[90vw] pt-6">
-        <div className="w-100 relative mb-6">
-          <NavbarLogo className="h-[90px] w-fit" />
-          <img
-            className="max-lg:hidden absolute right-0 top-0"
-            src="/images/transfer/invoice.png"
-            alt=""
-            aria-hidden={true}
-          />
+      <Breadcrumbs breadcrumbs={breadcrumbs} />
+      <div className="container w-[638px] rounded-[20px] border border-grey p-8 shadow-2xl">
+        <div className="my-3 flex flex-col items-center p-3">
+          <div className="my-2 flex h-[70px] w-[70px] rounded-full bg-success">
+            <img
+              src="/images/icons/checklist.png"
+              alt="Berhasil"
+              className="m-auto"
+            />
+          </div>
+          <h1 className="text-lg font-bold">Tranksaksi Berhasil</h1>
+          <div className="my-2 flex items-center gap-[15px] text-lg text-dark-grey">
+            <p>10 Juni 2024</p>
+            <div className="h-[10px] w-[10px] rounded-full bg-dot-grey"></div>
+            <p>10.30</p>
+          </div>
         </div>
-        <h1 className="text-md-display font-bold">Invoice Transfer</h1>
-        <h2 className="text-xs-display font-regular">
-          Invoice ini merupakan bukti pembayaran yang sah
-        </h2>
-        <div className="mt-6 flex max-w-[50vw] gap-4">
-          <div className="receipt-label w-1/2">
-            <div className="">Rekening Sumber</div>
-            <div className="">Rekening Tujuan</div>
-            <div className="">Nama Penerima</div>
-            <div className="">Nominal Transfer</div>
-            <div className="">Biaya Admin</div>
-            <div className="">Catatan</div>
-            <div className="">Tanggal Transaksi</div>
-            <div className="font-bold">Total</div>
+
+        <div className="border-b-4 border-grey"></div>
+
+        <div className="mt-3 flex gap-[40px] p-3 text-lg">
+          <div className="text-muted-black">
+            <p className="my-3">Rekening Sumber</p>
+            <p className="my-3">Rekening Tujuan</p>
+            <p className="my-3">Nama penerima</p>
+            <p className="my-3">Nominal Transfer</p>
+            <p className="my-3">Biaya Admin</p>
+            <p className="my-3">Catatan</p>
+            <p className="my-3">Total</p>
           </div>
-          <div className="receipt-value w-1/2">
-            <div className="">{senderAccount}</div>
-            <div className="">{receiverAccount}</div>
-            <div className="">{receiverName}</div>
-            <div className="">{formatRupiah(amount)}</div>
-            <div className="">{formatRupiah(adminFee)}</div>
-            <div className="">{note}</div>
-            <div className="">{transactionDate}</div>
-            <div className="font-bold">{formatRupiah(total)}</div>
+          <div className="mx-auto text-dark-grey">
+            <p className="my-3">8923445590</p>
+            <p className="my-3">2448901238</p>
+            <p className="my-3">John</p>
+            <p className="my-3">Rp 100.000</p>
+            <p className="my-3">Rp 0</p>
+            <p className="my-3">Bayar makanan</p>
+            <p className="my-3">Rp 100.000</p>
           </div>
+        </div>
+
+        <div className="border-b-2 border-grey"></div>
+
+        <div className="mb-5 flex gap-[144px] p-3 text-lg">
+          <div className="text-muted-black">
+            <p className="my-3">Catatan</p>
+            <p className="my-3">Total</p>
+          </div>
+          <div className="mx-auto text-dark-grey">
+            <p className="my-3">Bayar makanan</p>
+            <p className="my-3">Rp 100.000</p>
+          </div>
+        </div>
+      </div>
+      <div className="my-5 flex p-3">
+        <div className="flex gap-7 self-center">
+          <Button
+            color="primary-dark-blue"
+            className="h-[52] w-[167px] rounded-[30px] border border-primary-dark-blue bg-white px-10 py-3 font-bold text-primary-dark-blue"
+          >
+            Kembali
+          </Button>
+          <Button
+            color="primary-dark-blue"
+            className="h-[52] w-[167px] rounded-[30px] border border-primary-dark-blue px-10 py-3 font-bold text-white"
+          >
+            <img src="/images/icons/download.svg" alt="" /> Unduh
+          </Button>
         </div>
       </div>
     </>
