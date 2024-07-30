886686ae41777a640a25ea1ef33e829b505809a6
deliyaizztl <deliyaizztl@gmail.com>
2024-07-30 09:34:29 +0700

    lupa-password-email

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        black40: '#00000066',
+        darkblue: '#0A3967',
+        lightblue: '#E4EDFF'
+      },
+    },
+  },
+  plugins: [],
+}
+
