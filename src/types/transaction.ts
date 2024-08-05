7ba3e16e70590f33c68f9611bb3625f8517b442a
Chery <cherylene40@gmail.com>
2024-08-05 13:30:45 +0700

    chore: Refactor code in dashboard and login

---
+++ src/types/transaction.ts
@@ -0,0 +1,36 @@
+interface TransactionProps {
+  transaction_id: string;
+  from: {
+    account_id: string;
+    owner_name: string;
+    account_number: string;
+  };
+  to: {
+    account_id: string;
+    owner_name: string;
+    account_number: string;
+  };
+  transaction_date: string;
+  amount: number;
+  admin_fee: number;
+  total: number;
+  note: number;
+  type: string;
+}
+
+interface TransactionsProps {
+  transactions: TransactionProps[] | null;
+}
+
+interface TransferFormValue {
+  account_id: string;
+  beneficiary_owner_name: string;
+  beneficiary_account: string;
+  beneficiary_name: string;
+  amount: number;
+  transaction_date: string; // Format: 'YYYY-MM-DD HH:mm:ss.SSS'
+  note: string | null;
+  is_saved: boolean;
+}
+
+export type { TransactionsProps, TransactionProps, TransferFormValue };
--- src/utils/getUserData.ts
+++ src/utils/getUserData.ts
@@ -3,7 +3,7 @@ import { Api } from '@/api/api';
 export const getAccountId = async (
   api: Api,
   accountNumber: string,
-  token: string
+  token: string | null
 ) => {
   try {
     const response = await api.get(
@@ -17,6 +17,24 @@ export const getAccountId = async (
     return response.data.data.account_id;
   } catch (error) {
     console.error('Error fetching account ID:', error);
+    throw error;
+  }
+};
+
+export const getAccountNumber = async (
+  api: Api,
+  userId: string | null,
+  token: string | null
+) => {
+  try {
+    const response = await api.get(`/api/bank-accounts/user/${userId}`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    return response.data.data.account_number;
+  } catch (error) {
+    console.error('Error fetching account number:', error);
     throw error;
   }
 };
