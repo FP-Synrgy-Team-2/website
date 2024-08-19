72f61140464b83c2015e76d5852afcfcb5510448
puzul <ymjonathangp2@gmail.com>
2024-08-19 20:46:49 +0700

    added responsive - history page responsive - added "transaction result" modal for mobile view

--- src/components/History/TransactionData.tsx
+++ src/components/History/TransactionData.tsx
@@ -1,14 +1,25 @@
-import { ButtonPrimary } from '@/components';
+import { ButtonPrimary, ButtonSecondary } from '@/components';
 import { TransactionProps } from '@/types/transaction';
 import { useNavigate } from 'react-router-dom';
 import formatRupiah from '@/utils/formatRupiah';
 
 interface TransactionDataProps {
   transaction: TransactionProps | null;
+  setActiveTransaction: React.Dispatch<
+    React.SetStateAction<TransactionProps | null>
+  >;
 }
 
-function TransactionData({ transaction }: TransactionDataProps) {
+function TransactionData({
+  transaction,
+  setActiveTransaction,
+}: TransactionDataProps) {
   const navigate = useNavigate();
+
+  const handleClose = () => {
+    setActiveTransaction(null);
+  };
+
   const handleDownload = () => {
     if (transaction)
       navigate(`/transfer/invoice/${transaction.transaction_id}`);
@@ -28,9 +39,9 @@ function TransactionData({ transaction }: TransactionDataProps) {
     <>
       {transaction && (
         <div className="sticky top-[20px] flex w-full flex-col items-center gap-5">
-          <div className="flex h-max items-center justify-center rounded-[20px] bg-white px-[30px] py-[60px] shadow-[0_4px_5px_5px_#EAF6FF]">
-            <div className="flex flex-col gap-y-5">
-              <div className="flex w-[447px] flex-col items-center">
+          <div className="flex h-max items-center justify-center rounded-[20px] bg-white px-[30px] py-[60px] shadow-[0_4px_5px_5px_#EAF6FF] xl:px-5 xl:py-[45px] sm:px-[15px] sm:py-[30px]">
+            <div className="flex flex-col items-center gap-y-5">
+              <div className="flex w-[447px] flex-col items-center xl:w-max">
                 <div className="flex h-12.5 w-12.5 items-center justify-center rounded-[25px] bg-success">
                   <img
                     src="/images/icons/checklist.png"
@@ -52,7 +63,7 @@ function TransactionData({ transaction }: TransactionDataProps) {
                 </div>
               </div>
               <div className="h-0 w-full rounded-[3px] border-[3px] border-neutral-03 border-opacity-30"></div>
-              <div className="grid grid-cols-[170px_126px] gap-x-[7.5px] gap-y-[15px] px-[30px] text-xl-body">
+              <div className="grid grid-cols-[170px_126px] gap-x-[7.5px] gap-y-[15px] px-[30px] text-xl-body sm:gap-x-[2.5px] sm:text-md-body">
                 <span className="text-muted-black">Rekening Sumber</span>
                 <span className="text-dark-grey">
                   {transaction.from.account_number}
@@ -82,13 +93,21 @@ function TransactionData({ transaction }: TransactionDataProps) {
               </div>
             </div>
           </div>
-          <ButtonPrimary
-            className="flex items-center gap-5 rounded-[10px]"
-            onClick={handleDownload}
-          >
-            <img src="/images/icons/download.svg" alt="" />
-            <span>Unduh</span>
-          </ButtonPrimary>
+          <div className="flex justify-center gap-x-4">
+            <ButtonSecondary
+              onClick={handleClose}
+              className="hidden bg-white lg:flex"
+            >
+              Tutup
+            </ButtonSecondary>
+            <ButtonPrimary
+              className="flex items-center gap-5 rounded-[10px]"
+              onClick={handleDownload}
+            >
+              <img src="/images/icons/download.svg" alt="" />
+              <span>Unduh</span>
+            </ButtonPrimary>
+          </div>
         </div>
       )}
     </>
--- src/components/History/TransactionsList.tsx
+++ src/components/History/TransactionsList.tsx
@@ -30,23 +30,23 @@ function TransactionsList({
         className={`flex cursor-pointer flex-col rounded-[10px] border border-black border-opacity-40 px-[11px] py-2 ${activeTransaction?.transaction_id === transaction.transaction_id ? 'bg-primary-light-blue' : ''} `}
       >
         <div className="flex gap-x-2.5">
-          <div className="flex h-[30px] w-[30px] items-center justify-center rounded-[15px] bg-primary-light-blue">
+          <div className="flex h-[30px] w-[30px] items-center justify-center rounded-[15px] bg-primary-light-blue xl:h-5 xl:w-5">
             <img src="/images/icons/arrow-up-down.svg" alt="" />
           </div>
-          <span className="text-2xl font-bold">
+          <span className="text-2xl font-bold xl:text-xl">
             {transaction.type === 'Pengeluaran' ? 'Transfer' : 'Terima Uang'}
           </span>
         </div>
 
-        <div className="flex justify-between gap-x-[5px]">
-          <span className="text-xl-body">
+        <div className="flex justify-between gap-x-[5px] text-xl-body xl:text-md-body">
+          <span>
             BCA -{' '}
             {transaction.type === 'Pengeluaran'
               ? transaction.to.account_number
               : transaction.from.account_number}
           </span>
-          <span className="text-xl-body">{`${transaction.type === 'Pengeluaran' ? '-' : '+'}${formatRupiah(transaction.total)}`}</span>
-          <span className="text-xl-body text-neutral-03">
+          <span>{`${transaction.type === 'Pengeluaran' ? '-' : '+'}${formatRupiah(transaction.total)}`}</span>
+          <span className="text-neutral-03">
             {returnLocalDateAndTime(transaction.transaction_date).localDate}{' '}
             {returnLocalDateAndTime(transaction.transaction_date).localTime}
           </span>
